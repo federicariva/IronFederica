cafc1eca3797f5075595e351828c5fcc818b6dbd
federicariva <federicayriva@gmail.com>
2022-05-04 17:41:25 +0200

    lab

---
+++ HI_LABS/3.week/LAB_3.3_SQL_queries.sql
@@ -0,0 +1,27 @@
+#Using the rental table, find out how many rentals were processed by each employee.
+SELECT 
+    staff_id, COUNT(rental_id)
+FROM
+    rental
+GROUP BY staff_id;
+
+#Using the film table, find out how many films were released.
+SELECT 
+    COUNT(DISTINCT film_id)
+FROM
+    film;
+
+#Using the film table, find out how many films there are of each rating.
+-- Sort the results in descending order of the number of films.
+SELECT 
+    rating, COUNT(film_id) AS filmno
+FROM
+    film
+GROUP BY rating
+ORDER BY filmno DESC;
+
+#Which kind of movies (rating) have a mean duration of more than two hours?
+select rating, avg(length)>120 from film
+group by rating;
+#having avg(length) > 120;
+
